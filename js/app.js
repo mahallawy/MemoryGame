a1a84fdc572078f8fbfec36e7907c8d1668b397a
Ahmad Mahallawy <ahmad.mahallawy@htomail.com>
2018-03-29 20:30:06 +0200

    changed stars rating for moves number

--- js/app.js
+++ js/app.js
@@ -189,13 +189,13 @@ function moveCounter() {
     startTimer();
   }
   // setting rates based on moves
-  if (moves > 9 && moves < 14) {
+  if (moves > 8 && moves < 14) {
     for (i = 0; i < 3; i++) {
       if (i > 1) {
         stars[i].style.visibility = "collapse";
       }
     }
-  } else if (moves > 16) {
+  } else if (moves > 12) {
     for (i = 0; i < 3; i++) {
       if (i > 0) {
         stars[i].style.visibility = "collapse";
